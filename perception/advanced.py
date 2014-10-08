ded4c3288204f8d8f0856196b364b2491d3c69ae
Petr Devaikin <p.devaikin@gmail.com>
2014-10-08 23:51:36 +0400

    hue perception

--- perception/advanced.py
+++ perception/advanced.py
@@ -3,7 +3,7 @@ from .base import BasePerception
 
 class SimplePerception(BasePerception):
     def _convert_pixel(self, pixel):
-        return sum(pixel) / 3
+        return 0.299 * pixel[0] + 0.587 * pixel[1] + 0.114 * pixel[2]
 
     def _get_converter(self, line):
         bw_line = [self._convert_pixel(pixel) for pixel in line]
@@ -13,4 +13,17 @@ class SimplePerception(BasePerception):
 
 class HuePerception(BasePerception):
     def _convert_pixel(self, pixel):
-        return pixel[0]
+        r = pixel[0] / 255.0
+        g = pixel[1] / 255.0
+        b = pixel[2] / 255.0
+        c_max = max((r, g, b))
+        delta = c_max - min((r, g, b))
+        if delta == 0:
+            hue = 0
+        elif r == c_max:
+            hue = (g - b) / delta
+        elif g == c_max:
+            hue = 2 + (b - r) / delta
+        else:
+            hue = 4 + (r - g) / delta
+        return hue * 60 if hue > 0 else hue * 60 + 360
