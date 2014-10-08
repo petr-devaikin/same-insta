467581dc2d05e28e1d0a1697feb93e16a84414ab
Petr Devaikin <p.devaikin@gmail.com>
2014-10-08 21:19:00 +0400

    base perception class

--- perception.py
+++ perception.py
@@ -1,16 +1,13 @@
-from PIL import Image
+from perception.base_perception import BasePerception
 
 def simple_perception(file_name):
     width = 8
     height = 8
     
-    image = Image.open(file_name)
-    small_grey = image.resize((width, height), Image.ANTIALIAS).convert('L')
-    line = [image.getpixel((x, y))[0] for x in range(width) for y in range(height)]
-    average = sum(line) / width / height
-    return map(lambda p: p > average, line)
+    p = BasePerception(width, height)
+    return p.get_hash(file_name)
 
 
 if __name__ == '__main__':
     res = simple_perception('test.jpg')
-    print sum(res)
+    print res
